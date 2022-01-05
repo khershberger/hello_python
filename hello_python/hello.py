6a3d9e67a1aaaa886485a562052f0e82d14b2d41
Kyle Hershberger <kyle.hershberger@gmail.com>
2022-01-05 12:08:39 -0500

    Add example code for setupClass() setUp()

--- hello_python/hello.py
+++ hello_python/hello.py
@@ -1,5 +1,24 @@
+class HelloClass(object):
+    def __init__(self):
+        self.a = None
+        self.count = 0
+
+    def say_hello(sself):
+        print("Hello world!")
+    
+    def increment(self):
+        self.count += 1
+        return True
+    
+    def set_count(self, c):
+        self.count = c
+    
+    def get_count(self):
+        return self.count
+
 def main():
-    print('Hello World')
+    hc = HelloClass()
+    hc.say_hello()
     return 0
 
 def return_true():
--- hello_python/tests/test_example_unittest.py
+++ hello_python/tests/test_example_unittest.py
@@ -14,7 +14,36 @@ class TestStuff(unittest.TestCase):
         self.assertTrue(hello.return_true())
 
     def test_assert_false(self):
-        self.assertFalse(hello.return_true())
+        self.assertFalse(not(hello.return_true()))
+
+class TestClass(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        print("setUpClass")
+        cls.c = hello.HelloClass()
+        cls.is_class_setup = True
+
+    def setUp(self):
+        print("setUp")
+        self.c.set_count(5)
+        self.is_setup = True
+        
+    def test_class(self):
+        self.assertTrue(self.is_class_setup)
+        self.assertTrue(self.is_setup)
+        self.assertEqual(self.c.get_count(), 5)
+        self.c.increment()
+        self.assertEqual(self.c.get_count(), 6)
+        
+    def test_two(self):
+        self.assertEqual(self.c.get_count(), 5)
+
+    def tearDown(self):
+        print("tearDown")
+    
+    @classmethod
+    def tearDownClass(cls):
+        print("tearDownClass")
 
 if __name__ == '__main__':
     unittest.main()
